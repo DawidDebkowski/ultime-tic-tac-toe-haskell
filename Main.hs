e888a4b052f8499dae38dbff4b80f030c67d58cc
blazejpawluk <bpawluk05@gmail.com>
2025-06-03 11:22:50 +0200

    parse move

--- Main.hs
+++ Main.hs
@@ -1,6 +1,14 @@
 module Main (other) where
 
 -- parsowanie ruchu
+parseMove :: String -> Maybe(Int, Int)
+parseMove s = case words s of 
+	[b,c]
+		| all valid [b,c]
+		-> let smallBoardIndex = read b-1; cellIndex = c-1 in Just (smallBoardIndex, cellIndex)
+		_ -> Nothing
+	where
+		valid t = lenght t == 1 && head t 'elem' ['1'..'9']
 
 -- zmiana gracza
 other :: Player -> Player
