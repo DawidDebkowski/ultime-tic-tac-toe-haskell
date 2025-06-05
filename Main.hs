59da87e19e16df3da10556c7fa964f84ac4558d8
Dawid Dębkowski <dawid.debo2.pl@gmail.com>
2025-06-05 18:40:01 +0200

    cos tam graja

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 module Main where
 import State
 import Board
+import MiniMax ( StateMove(move, eval), findBestMove )
 
 -- parsowanie ruchu
 parseMove :: String -> Maybe (Int, Int)
@@ -24,13 +25,33 @@ gameLoop st = do
     Nothing       -> do
       putStrLn $ "Current: " ++ show (current st)
       putStrLn $ "Play in sub-board: " ++ maybe "any" (show . (+1)) (nextIndex st)
-      putStrLn "Enter move as: sub-board(1-9) cell(1-9), e.g. '5 3':"
-      line <- getLine
-      case parseMove line of
-        Just mv | checkMove st mv -> gameLoop (makeMove st mv)
-        _ -> putStrLn "Invalid move, try again." >> gameLoop st
+      if current st == O -- AI plays as O
+        then do
+          putStrLn "AI is thinking..."
+          let aiMove = findBestMove st 8 False -- 4 is the search depth, adjust as needed
+          putStrLn $ "AI plays: " ++ show (fst (move aiMove) + 1) ++ " " ++ show (snd (move aiMove) + 1) ++ " " ++ show (eval aiMove)
+          gameLoop (makeMove st (move aiMove))
+        else do
+          putStrLn "Enter move as: sub-board(1-9) cell(1-9), e.g. '5 3':"
+          line <- getLine
+          case parseMove line of
+            Just mv | checkMove st mv -> gameLoop (makeMove st mv)
+            _ -> putStrLn "Invalid move, try again." >> gameLoop st
+
+botGameLoop :: State -> IO ()
+botGameLoop st = do
+  putStrLn $ showBoard st
+  case checkBigBoard (board st) of
+    Just (Just p) -> putStrLn $ "Player " ++ show p ++ " wins!"
+    Just Nothing  -> putStrLn "Game is a draw!"
+    Nothing       -> do
+      putStrLn $ "Current: " ++ show (current st)
+      putStrLn $ "Play in sub-board: " ++ maybe "any" (show . (+1)) (nextIndex st)
+      putStrLn $ "AI (" ++ show (current st) ++ ") is thinking..." ++ show (current st == X)
+      let aiMove = findBestMove st 4 (current st == X) -- Adjust depth for speed/performance
+      putStrLn $ "AI (" ++ show (current st) ++ ") plays: " ++ show (fst (move aiMove) + 1) ++ " " ++ show (snd (move aiMove) + 1) ++ " | eval: " ++ show (eval aiMove)
+      botGameLoop (makeMove st (move aiMove))
 
 -- main
 main:: IO();
-main = do
-  gameLoop startingState;
+main = botGameLoop startingState;
