977708f625efecfab18bc4ff9a28a520b4807797
Dawid Dębkowski <dawid.debo2.pl@gmail.com>
2025-06-06 16:15:01 +0200

    nowy starty gameloop

--- Main.hs
+++ Main.hs
@@ -20,9 +20,21 @@ parseMove s = case words s of
   where
     valid t = length t == 1 && head t `elem` ['1'..'9']
 
+-- główna pętla (main loop)
 gameLoop :: State -> IO ()
-gameLoop st = putStrLn (showBoard st)
-
+gameLoop st = do
+  putStrLn $ showBoard st
+  case checkBigBoard (board st) of
+    Just (Just p) -> putStrLn $ "Player " ++ show p ++ " wins!"
+    Just Nothing  -> putStrLn "Game is a draw!"
+    Nothing       -> do
+      putStrLn $ "Current: " ++ show (current st)
+      putStrLn $ "Play in sub-board: " ++ maybe "any" (show . (+1)) (nextIndex st)
+      putStrLn "Enter move as: sub-board(1-9) cell(1-9), e.g. '5 3':"
+      line <- getLine
+      case parseMove line of
+        Just mv | checkMove st mv -> gameLoop (makeMove st mv)
+        _ -> putStrLn "Invalid move, try again." >> gameLoop st
 
 -- główna pętla (main loop)
 oneBotGameLoop :: State -> Player -> IO ()
